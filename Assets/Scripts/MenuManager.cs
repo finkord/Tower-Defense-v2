b1b7ac81bdd23b13b3887c0e0e3cc341881c2121
finkord <devfinkord@gmail.com>
2026-05-17 03:00:21 +0300

    added map2, fixed tiles

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,31 +3,59 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    [Header("Scene Settings")]
-    public string gameSceneName = "MainScene"; // Change to your actual game scene name
+    [Header("UI Panels")]
+    public GameObject mapSelectionPanel;
 
-    public void Start10WaveMode()
+    private void Start()
+    {
+        if (mapSelectionPanel != null)
+        {
+            mapSelectionPanel.SetActive(false);
+        }
+    }
+
+    public void Select10WaveMode()
     {
         // 0 = Finite Mode (10 Waves)
         PlayerPrefs.SetInt("GameMode", 0);
         PlayerPrefs.Save();
-        SceneManager.LoadScene(gameSceneName);
+        ShowMapSelection();
     }
 
-    public void StartEndlessMode()
+    public void SelectEndlessMode()
     {
         // 1 = Endless Mode
         PlayerPrefs.SetInt("GameMode", 1);
         PlayerPrefs.Save();
-        SceneManager.LoadScene(gameSceneName);
+        ShowMapSelection();
+    }
+
+    private void ShowMapSelection()
+    {
+        if (mapSelectionPanel != null)
+        {
+            mapSelectionPanel.SetActive(true);
+        }
+    }
+
+    // New method to close map panel and return to mode selection
+    public void CloseMapSelection()
+    {
+        if (mapSelectionPanel != null)
+        {
+            mapSelectionPanel.SetActive(false);
+        }
+    }
+
+    public void LoadMap(string mapSceneName)
+    {
+        SceneManager.LoadScene(mapSceneName);
     }
 
     public void ExitGame()
     {
-        // Works in built application
         Application.Quit();
         
-        // Only for testing in Unity Editor
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
